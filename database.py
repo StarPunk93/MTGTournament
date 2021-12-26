c05aee0d2f9a8a225df2c3bf66ad0e6d02288100
Joe Christoffersen <j.christoffersen@kbit.de>
2021-12-26 01:30:34 +0100

    Added Matches and Match Creation. Changed point calculation. Added funny CSS stuff.

--- database.py
+++ database.py
@@ -32,5 +32,28 @@ class Database:
         return(True)
 
     def get_wins(self, playername):
-        self.cursor.execute(f'SELECT COUNT(`Match`.`winner`) from `Match` INNER JOIN `Players` ON `Match`.`winner`=`Players`.id WHERE `Players`.name = "{playername}"')
-        return self.cursor.fetchall()
+        wins = self.query(f'SELECT COUNT(`Match`.player1) FROM `Match` INNER JOIN `Players` ON `Match`.player1=`Players`.id WHERE `Players`.name = "{playername}" AND `Match`.winner = "1"')[0][0]
+        wins = wins + self.query(f'SELECT COUNT(`Match`.player2) FROM `Match` INNER JOIN `Players` ON `Match`.player2=`Players`.id WHERE `Players`.name = "{playername}" AND `Match`.winner = "2"')[0][0]
+        print(wins)
+        #self.cursor.execute(f'SELECT COUNT(`Match`.`winner`) from `Match` INNER JOIN `Players` ON `Match`.`winner`=`Players`.id WHERE `Players`.name = "{playername}"')
+        return(wins)
+
+    def new_match(self, player1id, player2id):
+        self.cursor.execute(f'INSERT INTO `Match` (`player1`, `winner`, `player2`) VALUES ({player1id}, 0, {player2id})')
+        self.db.commit()
+        return(True)
+
+    def delete_all_matches(self):
+        self.cursor.execute('TRUNCATE `Match`')
+        self.db.commit()
+        return(True)
+
+    def set_matchwinner(self, matchid, winnerid):
+        #print(winnerid)
+        #print(matchid)
+        #print(self.query(f'SELECT player1 FROM `Match` WHERE player1 = "{winnerid}" AND id = "{matchid}"'))
+        if self.query(f'SELECT player1 FROM `Match` WHERE player1 = "{winnerid}" AND id = "{matchid}"') is not ():
+            self.cursor.execute(f'UPDATE `Match` SET winner = "1" WHERE id = "{matchid}"')
+        if self.query(f'SELECT player2 FROM `Match` WHERE player2 = "{winnerid}" AND id = "{matchid}"') is not ():
+            self.cursor.execute(f'UPDATE `Match` SET winner = "2" WHERE id = "{matchid}"')
+        self.db.commit()
--- mtg.py
+++ mtg.py
@@ -17,7 +17,41 @@ class MTG:
             self.db.add_player(playername)
             return(True)
 
+    def delete_player(self, playername):
+        if self.db.player_exists(playername) is True:
+            self.db.query(f'DELETE FROM Players WHERE `name`="{playername}"')
+
     def get_points(self, playername):
         wins = self.db.get_wins(playername)
-        points = wins[0][0]
+        points = wins
         return(points)
+
+    def get_matches(self):
+        ### Output format: tuple(player1, player2, winner)
+        matches = self.db.query('SELECT `Players`.name, `match`.player2, `match`.winner, `match`.id FROM `Match` INNER JOIN `Players` ON `Match`.player1=`Players`.id')
+        matches_with_names = []
+        for match in matches:
+            player2name = self.db.query(f'SELECT `Players`.name FROM `Players` WHERE `id`="{match[1]}"')
+            matches_with_names.append([match[0], player2name[0][0], match[2], match[3] ])
+        #print(matches_with_names)
+        return(matches_with_names)
+
+    def create_all_matches(self):
+        playercounter = 0
+        players = self.get_players()
+        for player1 in players:
+            playercounter = playercounter + 1
+            for player2 in self.get_players():
+                if player2 != player1:
+                    if player2 not in players[0:playercounter]:
+                        #print(str(player1) + ' ' + str(player2))
+                        self.db.new_match(player1[0], player2[0])
+        return(True)
+
+    def delete_all_matches(self):
+        self.db.delete_all_matches()
+        return(True)
+
+    def set_matchwinner(self, matchid, winner):
+        winnerid = self.db.query(f'SELECT `Players`.id FROM `Players` WHERE name = "{winner}"')[0][0]
+        self.db.set_matchwinner(matchid, winnerid)
